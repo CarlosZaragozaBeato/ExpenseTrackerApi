bf552d709043a5b09593d10b15eba8aaabf72721
carlos <carloszaragozabeato15@gmail.com>
2024-12-21 08:59:46 +0100

    s

--- src/main/java/com/carloszaragoza/expensetracker/security/JwtUtil.java
+++ src/main/java/com/carloszaragoza/expensetracker/security/JwtUtil.java
@@ -11,6 +11,29 @@ public class JwtUtil {
     private final String SECRET_KEY = "";
 
 
+    public String generateToken(String email){
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
 
+    public String extractEmail(String token){
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+    public boolean validateToken(String token){
+        try{
+            Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+            return true;
+        }catch (JwtException e){
+            return false;
+        }
+    }
 
 }
